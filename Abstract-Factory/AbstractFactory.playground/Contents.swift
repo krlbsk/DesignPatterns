cd6954b748dbfd71cd8fddecb28941355e0f62d4
Karol Bielski <imaginedthinker@gmail.com>
2020-10-23 18:31:45 +0200

    Add initial playground

---
+++ Abstract-Factory/AbstractFactory.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addSubview(button)
+        return view
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 20)
+        button.setTitle("Hello World!", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    override func loadView() {
+        self.view = contentView
+    }
+}
+
+PlaygroundPage.current.liveView = MyViewController()
