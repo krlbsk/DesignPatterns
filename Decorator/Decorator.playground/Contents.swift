16e16b3cc5f4bdc453349c6c02966f97f9ff8f7d
Karol Bielski <imaginedthinker@gmail.com>
2020-10-23 17:36:37 +0200

    Change label to button

--- Decorator/Decorator.playground/Contents.swift
+++ Decorator/Decorator.playground/Contents.swift
@@ -6,16 +6,16 @@ class MyViewController : UIViewController {
     private lazy var contentView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
-        view.addSubview(label)
+        view.addSubview(button)
         return view
     }()
     
-    private let label: UILabel = {
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        return label
+    private let button: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 20)
+        button.setTitle("Hello World!", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
     }()
     
     override func loadView() {
