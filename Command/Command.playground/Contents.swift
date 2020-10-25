99a6d29d5b3882c53fd95fd008e7776875b30cdd
Karol Bielski <imaginedthinker@gmail.com>
2020-10-25 12:53:04 +0100

    Add initial playground

---
+++ Command/Command.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+import PlaygroundSupport
+
+class MyViewController: UIViewController {
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addSubview(label)
+        return view
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        return label
+    }()
+    
+    override func loadView() {
+        self.view = contentView
+    }
+}
+
+PlaygroundPage.current.liveView = MyViewController()
