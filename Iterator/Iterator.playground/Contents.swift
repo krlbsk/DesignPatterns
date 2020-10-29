dbaceb9f558a2aae9c87b2872f830e48b2f4768f
Karol Bielski <imaginedthinker@gmail.com>
2020-10-29 17:20:16 +0100

    Add every two index list iterator

--- Iterator/Iterator.playground/Contents.swift
+++ Iterator/Iterator.playground/Contents.swift
@@ -1,5 +1,5 @@
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let numbersIterator = numbers.iterator
+let numbersIterator = numbers.everyTwoIterator
 
 while !numbersIterator.isDone() {
     guard let number = numbersIterator.next() else { break }
--- Iterator/Iterator.playground/Sources/Array+iterator.swift
+++ Iterator/Iterator.playground/Sources/Array+iterator.swift
@@ -2,4 +2,8 @@ public extension Array {
     var iterator: ListIterator<Element> {
         ListIterator(list: self)
     }
+    
+    var everyTwoIterator: EveryTwoListIterator<Element> {
+        EveryTwoListIterator(list: self)
+    }
 }
