dbb87aa8e797fa6cf54ba2980a614769f72a64ad
Karol Bielski <imaginedthinker@gmail.com>
2020-11-02 18:02:58 +0100

    Change initial playground

--- Mediator/Mediator.playground/Contents.swift
+++ Mediator/Mediator.playground/Contents.swift
@@ -1,1 +1,33 @@
+import UIKit
+import PlaygroundSupport
 
+class MyViewController: UIViewController {
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addSubview(label)
+        view.addSubview(button)
+        return view
+    }()
+    
+    private var label: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "No data"
+        label.textColor = .black
+        return label
+    }()
+    
+    private var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 100, y: 250, width: 200, height: 20)
+        button.setTitle("Sync", for: .normal)
+        return button
+    }()
+    
+    override func loadView() {
+        view = contentView
+    }
+}
+
+PlaygroundPage.current.liveView = MyViewController()
